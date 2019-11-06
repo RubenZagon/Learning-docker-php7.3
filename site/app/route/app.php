9d3df57d1d6a422cde65bca55eab77c3cc198857
Ruben <ruben.zagon@gmail.com>
2019-11-06 17:47:27 +0000

    Instalo correctamente las dependencias, y genero la primera función en route

--- site/app/route/app.php
+++ site/app/route/app.php
@@ -1,2 +1,16 @@
 <?php
 
+$aApp -> get ('/frase', function ($request, $response, $args) {
+    $filedir  = "../database/frases.txt";
+
+    # Obtiene el contenido de texto del fichero y lo desglosa en un array
+    $getFile = file($filedir);
+
+    # Genera un número aleatorio, midiendo la totalidad de frases que hay
+    $getMAX = count($getFile);
+    $random = mt_rand(0, $getMAX);
+
+    var_dump($request);
+
+    return $getFile[$random]
+});
