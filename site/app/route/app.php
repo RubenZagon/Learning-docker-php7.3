959e126be9a3c45f562edc8ac679e448e062ce18
Ruben <ruben.zagon@gmail.com>
2019-11-06 18:49:13 +0000

    Creo dos funciones en rout->app.php y funciona

--- site/app/route/app.php
+++ site/app/route/app.php
@@ -1,7 +1,8 @@
 <?php
 
-$aApp -> get ('/frase', function ($request, $response, $args) {
-    $filedir  = "../database/frases.txt";
+$aApp -> get ('/una-frase', function ($request, $response, $args) {
+
+    $filedir  = "../database/frases.txt";;
 
     # Obtiene el contenido de texto del fichero y lo desglosa en un array
     $getFile = file($filedir);
@@ -10,7 +11,24 @@ $aApp -> get ('/frase', function ($request, $response, $args) {
     $getMAX = count($getFile);
     $random = mt_rand(0, $getMAX);
 
-    var_dump($request);
+    return $getFile[$random];
+});
 
-    return $getFile[$random]
+$aApp -> get ('/todas-las-frases', function ($request, $response, $args) {
+
+    $filedir  = "../database/frases.txt";
+
+    # Obtiene el contenido de texto del fichero y lo desglosa en un array
+            $getFile = file($filedir);
+
+    # Imprime todas las frases en la página
+    echo "<h1> Todas las frases</h1>";
+
+    foreach ($getFile as $frase) {
+        echo "<br>$frase";
+    }
+
+    // d($getFile); // Descomentar para ver todas el array sin imprimirlo en la página
+
+    return;
 });
