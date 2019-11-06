a5101d36ec291f071525f374d68aefbe2c840a4e
Ruben <ruben.zagon@gmail.com>
2019-11-06 17:02:52 +0000

    Añado en el composer.json las dependencias de slim

--- site/public/index.php
+++ site/public/index.php
@@ -1,9 +1,16 @@
 <?php
-
+/*
+##############################################
   // Include Composer autoload
   require('../vendor/autoload.php');
 
   // Incluimos el archivo Frases.txt
   include("./frases.php");
 
+##############################################
+*/
+
+  require __DIR__ . '/../bootstrap/app.php';
+
+  $aApp -> run();
 ?>
