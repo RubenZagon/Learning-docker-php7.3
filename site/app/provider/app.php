4da0fbed0e95db6d9458426b852d8ba9db20edae
Ruben <ruben.zagon@gmail.com>
2019-11-07 17:25:35 +0000

    Configuramos twig y definimos la primera plantilla

--- site/app/provider/app.php
+++ site/app/provider/app.php
@@ -2,11 +2,24 @@
 
 $aContainer = $aApp -> getContainer();
 
-$aContainer['Frases_Controller'] = function ($Entrada_Container) {
-    return new \Controller\Frases_Controller($Entrada_Container);
+$aContainer['view'] = function ($cContainer){
+
+    $aConfig = $cContainer -> get ('config')['view'];
+
+    $vViews = new \Slim\Views\Twig( $aConfig['path'], $aConfig['twig']);
+
+
+    $vViews -> addExtension (new \Slim\Views\TwigExtension(
+        $cContainer -> router,
+        $cContainer -> request -> getUri ()
+    ));
 };
 
-$aContainer['db'] = function ($Entrada_Container) {
+$aContainer['Frases_Controller'] = function ($cContainer) {
+    return new \Controller\Frases_Controller($cContainer);
+};
 
-    $aConfig = $Entrada_Container -> get('config')['db'];
+$aContainer['db'] = function ($cContainer) {
+
+    $aConfig = $cContainer -> get('config')['db'];
 };
--- site/app/route/app.php
+++ site/app/route/app.php
@@ -1,7 +1,23 @@
 <?php
 
+use Psr\Container\ContainerInterface as Container,
+    Psr\Http\Message\ServerRequestInterface as Request,
+    Psr\Http\Message\ResponseInterface as Response;
+
 $aApp -> get ('/random-frase', Frases_Controller::Class . ':randomPhrase');
 
 $aApp -> get ('/todas-las-frases', Frases_Controller::Class . ':allPhrases');
 
 $aApp -> get ('/id-frase/{id}', Frases_Controller::Class . ':getPhraseByID');
+
+$aApp -> get ('/plantilla', function (Request $rRequest, Response $rResponse, $args){
+
+    $aParameters = [
+        'aPage' => [
+            'strTitle' => 'Bienvenido a Slim + Twig',
+            'strDescription' => 'Esta es una plantilla oficial de Slim + Twig'
+        ]
+    ];
+
+    return $this -> view -> render ($rResponse, 'plantilla.twig', $aParameters);
+});
