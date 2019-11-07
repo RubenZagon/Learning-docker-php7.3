ccc0814c6f1f6a17ef01f58dcc0b77b664e451f0
Ruben <ruben.zagon@gmail.com>
2019-11-07 16:13:44 +0000

    Corregido error con la declaración global de la variable

--- site/app/controller/Frases_Controller.php
+++ site/app/controller/Frases_Controller.php
@@ -13,20 +13,18 @@ class Frases_Controller {
 
     protected $atributo_Container;
 
-    const FILEDIR = '../database/frases.txt';
-
-    private $getFile = file(self::FILEDIR);
+    private $getFile;
 
     public function __construct (Container $entrada_Container) {
-        
+
+        # Obtiene el contenido de texto del fichero y lo desglosa en un array
+        $this -> getFile = file('../database/frases.txt');
+
         $this -> atributo_Container = $entrada_Container;
     }
 
 
     public function randomPhrase (Request $rRequest, Response $rResponse, $args ){
-
-        # Obtiene el contenido de texto del fichero y lo desglosa en un array
-        //$getFile = file(self::FILEDIR);
 
         # Genera un número aleatorio, midiendo la totalidad de frases que hay
         $getMAX = count($this -> getFile);
@@ -35,28 +33,24 @@ class Frases_Controller {
         return $this -> getFile[$random];
     }
 
-    public function allPhrases (Request $rRequest, Response $rResponse, $args){
 
-        # Obtiene el contenido de texto del fichero y lo desglosa en un array
-        $getFile = file(self::FILEDIR);
+    public function allPhrases (Request $rRequest, Response $rResponse, $args){
 
         # Imprime todas las frases en la página
         echo "<h1> Todas las frases</h1>";
 
-        foreach ($getFile as $frase) {
+        foreach ($this -> getFile as $frase) {
             echo "<br>$frase";
         }
 
         return;
     }
 
+
     public function getPhraseByID (Request $rRequest, Response $rResponse, $args){
 
-        # Obtiene el contenido de texto del fichero y lo desglosa en un array
-        $getFile = file(self::FILEDIR);
-
         # Mediante el argumento que le metemos, se selecciona el elemento del array, se le pone un -1 porque el array comienza el elemento 1. en la posición 0
-        return $getFile[$args["id"]-1];
+        return $this -> getFile[$args["id"]-1];
 
     }
 
